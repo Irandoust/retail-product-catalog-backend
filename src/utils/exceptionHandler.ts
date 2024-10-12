cee368a6e4d9c23ea5fefb74487006335ba920c2
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 15:15:31 +0200

    feat(utils): added exceptionHandler to have a reusable function for handling exceptions

---
+++ src/utils/exceptionHandler.ts
@@ -0,0 +1,15 @@
+import { StatusCodes } from 'http-status-codes';
+import { ServiceResponse } from '../api/models/serviceResponse';
+
+export const exceptionHandler = (
+  ex: unknown,
+  context: string,
+): ServiceResponse => {
+  const errorMessage = `${context}: ${(ex as Error).message}`;
+
+  return ServiceResponse.failure(
+    errorMessage ?? 'An error occurred while processing the request.',
+    null,
+    StatusCodes.INTERNAL_SERVER_ERROR,
+  );
+};
