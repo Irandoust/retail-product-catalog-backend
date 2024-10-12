8f2005d7fc5ea4f1066e57457a892bfbed9a455f
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 17:13:19 +0200

    feat(product): added getProducts method to the controller
    refactor(product): refactored addProduct to parse the body before passing the data to service

--- src/api/controllers/productController.ts
+++ src/api/controllers/productController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { ProductService } from '../services/productService';
 import { serviceResponseHandler } from '../../utils/serviceResponseHandler';
+import { paginationSchema } from '../schemas/paginationSchema';
+import { productSchema } from '../schemas/productSchema';
 
 export class ProductController {
   private productService: ProductService;
@@ -10,7 +12,15 @@ export class ProductController {
   }
 
   addProduct = (req: Request, res: Response) => {
-    const serviceResponse = this.productService.addProduct(req.body);
+    const { body } = productSchema.parse({ body: req.body });
+    const serviceResponse = this.productService.addProduct(body);
+    return serviceResponseHandler(serviceResponse, res);
+  };
+
+  getProducts = (req: Request, res: Response) => {
+    const { query } = paginationSchema.parse({ query: req.query });
+    const { page, limit } = query;
+    const serviceResponse = this.productService.getProducts(page, limit);
     return serviceResponseHandler(serviceResponse, res);
   };
 }
