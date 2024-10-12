a9ddf37dbc3b93d7c812abfe74427fd285d19648
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 13:54:22 +0200

    feat(server): added product router to server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { env } from './config';
+import { StatusCodes } from 'http-status-codes';
+import { productRouter } from './api/routes/productRouter';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/products', productRouter);
+
+app.use('*', (_req: Request, res: Response): void => {
+  res.status(StatusCodes.NOT_FOUND).send({ error: 'Not Found' });
+});
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on port ${env.PORT}`);
