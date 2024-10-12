c50db6f743a5171be06820d85b5711ff9a6ab1bf
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 17:06:57 +0200

    feat(product): added getProducts method to the product repository

--- src/api/repositories/productRepository.ts
+++ src/api/repositories/productRepository.ts
@@ -1,12 +1,17 @@
 import { Product } from '../models/productModel';
+import { products } from './productsData';
 
 export class ProductRepository {
-  private products: Product[] = [];
+  private products: Product[] = Array.from(products);
 
   addProduct = (product: Product): Product => {
     this.products.push(product);
 
     return product;
+  };
+
+  getProducts = (): Product[] => {
+    return this.products;
   };
 }
 export { Product };
