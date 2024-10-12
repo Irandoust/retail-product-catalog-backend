f015fa0bee5ba7969de036edf3e68cfa7871ffd1
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 17:09:59 +0200

    feat(product): added AddProductRequest to seperate models

--- src/api/models/productModel.ts
+++ src/api/models/productModel.ts
@@ -1,3 +1,11 @@
+export interface AddProductRequest {
+  name: string;
+  category: string;
+  description?: string;
+  price: number;
+  imageUrl: string;
+}
+
 export interface Product {
   id: string;
   name: string;
