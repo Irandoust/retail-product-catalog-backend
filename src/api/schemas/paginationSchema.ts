f1618dcc109c4cfe3c7cd7a2ed8f521d48c12b14
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 17:08:22 +0200

    feat(validation): added pagination schema for validation

---
+++ src/api/schemas/paginationSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const paginationSchema = z.object({
+  query: z
+    .object({
+      page: z.coerce.number().min(1).optional(),
+      limit: z.coerce.number().min(1).optional(),
+    })
+    .strict(),
+});
+
+export type PaginationQuery = z.infer<typeof paginationSchema>;
