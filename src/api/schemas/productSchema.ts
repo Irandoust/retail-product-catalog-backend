7ef2e8d62ee2aeef7f37540deb2168cc34346ec4
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-12 17:15:42 +0200

    refactor(validation): added strict method to body to bullet proof the validation

--- src/api/schemas/productSchema.ts
+++ src/api/schemas/productSchema.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
 export const productSchema = z.object({
-  body: z.object({
-    name: z.string().min(1, { message: 'Name is required' }),
-    category: z.string().min(1, { message: 'Category is required' }),
-    description: z.string().min(1, { message: 'Description is required' }),
-    price: z.number().min(0, { message: 'Price must be a positive number' }),
-    imageUrl: z.string().url({ message: 'Image is required' }),
-  }),
+  body: z
+    .object({
+      name: z.string().min(1, { message: 'Name is required' }),
+      category: z.string().min(1, { message: 'Category is required' }),
+      description: z.string().min(1, { message: 'Description is required' }),
+      price: z.number().min(0, { message: 'Price must be a positive number' }),
+      imageUrl: z.string().url({ message: 'Image is required' }),
+    })
+    .strict(),
 });
