26f81fa666a6e38e47d79ff45a62ac92e280888e
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 20:04:09 +0200

    feat(docs): added comments for the product models

--- src/api/models/productModel.ts
+++ src/api/models/productModel.ts
@@ -1,3 +1,12 @@
+/**
+ * Represents the data required to add a new product.
+ *
+ * @property {string} name - The name of the product.
+ * @property {string} category - The category to which the product belongs.
+ * @property {string} [description] - The description of the product.
+ * @property {number} price - The price of the product.
+ * @property {string} imageUrl - The URL of the product's image.
+ */
 export interface AddProductRequest {
   name: string;
   category: string;
@@ -6,6 +15,16 @@ export interface AddProductRequest {
   imageUrl: string;
 }
 
+/**
+ * Represents a product in the catalog.
+ *
+ * @property {string} id - The unique identifier for the product.
+ * @property {string} name - The name of the product.
+ * @property {string} category - The category to which the product belongs.
+ * @property {string} [description] - The description of the product.
+ * @property {number} price - The price of the product.
+ * @property {string} imageUrl - The URL of the product's image.
+ */
 export interface Product {
   id: string;
   name: string;
