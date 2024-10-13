45e2f34273d77bae738f83bb6cea51c752a9e8bc
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 20:13:27 +0200

    feat(docs): added comments to the ProductRepository

--- src/api/repositories/productRepository.ts
+++ src/api/repositories/productRepository.ts
@@ -2,18 +2,36 @@ import { Product } from '../models';
 import { products } from './productsData';
 
 export class ProductRepository {
+  // Initialize the repository with pre-existing product data
   private products: Product[] = Array.from(products);
 
+  /**
+   * Adds a new product to the repository.
+   *
+   * @param {Product} product - The product to be added.
+   * @returns {Product} The newly added product.
+   */
   addProduct = (product: Product): Product => {
     this.products.push(product);
 
     return product;
   };
 
+  /**
+   * Retrieves all products from the repository.
+   *
+   * @returns {Product[]} An array of all products.
+   */
   getProducts = (): Product[] => {
     return this.products;
   };
 
+  /**
+   * Retrieves a product by its unique ID.
+   *
+   * @param {string} id - The ID of the product to retrieve.
+   * @returns {Product | undefined} The product if found, otherwise undefined.
+   */
   getProductById = (id: string): Product | undefined => {
     const product = this.products.find((product) => product.id === id);
 
