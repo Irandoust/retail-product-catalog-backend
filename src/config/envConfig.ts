f543a27e94113cc496e50a9a9cfcd6b4c63d690a
Ali Irandoust <aliirand@WKMZT52E9C4E.global.publicisgroupe.net>
2024-10-13 19:54:38 +0200

    feat(docs): added comments for the env config

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -3,6 +3,18 @@ import { cleanEnv, str, host, port, num } from 'envalid';
 
 dotenv.config();
 
+/**
+ * Validates and cleans environment variables using envalid.
+ * Ensures that required environment variables are present, with sensible defaults if not explicitly set.
+ *
+ * - NODE_ENV: Specifies the environment ('development', 'production', or 'test').
+ * - HOST: The hostname where the server is hosted.
+ * - PORT: The port the application will listen on.
+ * - FUZZY_SEARCH_MAX_ALLOWED_DISTANCE: The maximum edit distance allowed for fuzzy searches.
+ * - COMMON_RATE_LIMIT_MAX_REQUESTS: The maximum number of requests allowed within a time window (for rate limiting).
+ * - COMMON_RATE_LIMIT_WINDOW_MS: The time window in milliseconds for rate limiting requests.
+ * - CORS_ORIGIN: The allowed origin for Cross-Origin Resource Sharing (CORS).
+ */
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({
     default: 'development',
